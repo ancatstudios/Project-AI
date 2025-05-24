d40ce92ca868f715bd5c3c444e4a6cdaf2ea8132
ancatstudios <ancatstudios@gmail.com>
2025-05-24 11:27:50 +0200

    Update maincode.py

--- maincode.py
+++ maincode.py
@@ -1,1 +1,12 @@
+from transformers import pipeline
+
+chatbot = pipeline("conversational", model="facebook/blenderbot-400M-distill")
+
+while True:
+    user_input = input("You: ")
+    if user_input.lower() in ["exit", "quit"]:
+        print("Chatbot: Goodbye!")
+        break
+    response = chatbot(user_input)
+    print("Chatbot:", response)
 
